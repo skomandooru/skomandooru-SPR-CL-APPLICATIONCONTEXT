c60a9707ef6a1078aca60e8a6c05f7ad0b580c42
tedbeast <tedbalashov@gmail.com>
2023-05-11 23:36:09 -0400

    skeleton

--- src/main/java/Lab/Application.java
+++ src/main/java/Lab/Application.java
@@ -10,15 +10,20 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * This is the Configuration class for this spring project - there are three beans, electricPoweredBean,
- * nuclearPoweredBean, and gasPoweredBean which are defined in this class, and they are based off of their respective
- * classes in the
+ * nuclearPoweredBean, and gasPoweredBean which are configured in this class. Notice that the Configuration annotation
+ * combined with the Bean annotation allows for the retrieval of objects as beans (objects managed by Spring.)
+ *
+ * Check out the contents of the "Lab" class to see how beans can be retrieved from the app context.
  */
 @Configuration
 public class Application {
     public static ApplicationContext applicationContext;
 
     public static void main(String[] args) {
-        applicationContext = new AnnotationConfigApplicationContext(Application.class);
+        Lab lab = new Lab();
+        System.out.println(lab.getElectricPoweredBean());
+        System.out.println(lab.getGasPoweredBean());
+        System.out.println(lab.getNuclearPoweredBean());
     }
     @Bean
     public ElectricEngine electricPoweredBean(){
